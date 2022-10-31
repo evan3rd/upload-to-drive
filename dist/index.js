38cf917d4a83afa698305638860f4f41bd25c6c6
Evan Wu <evan.wu@authme.com>
2022-10-31 16:51:07 +0800

    temp

---
+++ dist/index.js
@@ -0,0 +1,288 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(779);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 42:
+/***/ (function(module) {
+
+module.exports = eval("require")("googleapis");
+
+
+/***/ }),
+
+/***/ 94:
+/***/ (function(module) {
+
+module.exports = eval("require")("glob");
+
+
+/***/ }),
+
+/***/ 141:
+/***/ (function(module) {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = require("fs");
+
+/***/ }),
+
+/***/ 779:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const actions = __webpack_require__(141);
+const { google } = __webpack_require__(42);
+const fs = __webpack_require__(747);
+const glob = __webpack_require__(94);
+//const archiver = require('archiver');
+
+/** Google Service Account credentials  encoded in base64 */
+const credentials = actions.getInput('credentials', { required: true });
+/** Google Drive Folder ID to upload the file/folder to */
+const folder = actions.getInput('folder', { required: true });
+/** Glob pattern for the file(s) to upload */
+const target = actions.getInput('target', { required: true });
+/** Optional name for the zipped file */
+const name = actions.getInput('name', { required: false });
+/** Link to the Drive folder */
+const link = 'link';
+/* Link to file inside of folder */
+const fileLink = 'fileLink';
+
+const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+const scopes = ['https://www.googleapis.com/auth/drive'];
+const auth = new google.auth.JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes);
+
+const drive = google.drive({ version: 'v3', auth });
+
+const driveLink = `https://drive.google.com/drive/folders/${folder}`
+
+async function getJWT() {
+  const client = new JWT({
+    email: credentialsJSON.client_email,
+    key: credentialsJSON.private_key,
+    scopes: ['https://www.googleapis.com/auth/drive.file'],
+  });
+  const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
+  const res = await client.request({url});
+  console.log(res.data);
+}
+
+async function main() {
+  actions.setOutput(link, driveLink);
+
+//   const targets = glob.sync(target);
+
+//   if (targets.length === 1) {
+//     const filename = targets[0].split('/').pop();
+//     uploadToDrive(filename, targets[0]);
+//   } else {
+//     actions.info(`Multiple items detected for glob ${target}`);
+//     actions.info('Zipping items...');
+
+//     const filename = `${name}.zip`;
+
+// //     zipItemsByGlob(target, filename)
+// //       .then(() => {
+// //         action.info('Output to the actions build log ${filename}')
+// //         uploadToDrive(name, filename);
+// //       })
+// //       .catch(e => {
+// //         actions.error('Zip failed');
+// //         throw e;
+// //       });
+//   }
+
+    //uploadToDrive('README.md', 'README.md');
+
+
+    console.log(target);
+    console.log(driveLink);
+    //console.log(auth);
+    //console.log(drive);
+    //console.log(credentialsJSON);
+    //listFiles();
+    searchFile();
+
+    //const fi = fs.createReadStream('./README.md')
+    //console.log(fi)
+
+    var data = fs.readFileSync(target);
+    console.log(data.toString());
+
+    getJWT();
+}
+
+/**
+ * Zips files by a glob pattern and stores it in memory
+ * @param {string} glob Glob pattern to be matched
+ * @param {string} out Name of the resulting zipped file
+ */
+// function zipItemsByGlob(glob, out) {
+//   const archive = archiver('zip', {zlib: {level: 9}});
+//   const stream = fs.createWriteStream(out);
+
+//   return new Promise((resolve, reject) => {
+//     archive
+//       .glob(glob)
+//       .on('error', err => reject(err))
+//       .pipe(stream);
+
+//     stream.on('close', () => {
+//       actions.info(`Files successfully zipped: ${archive.pointer()} total bytes written`);
+//       return resolve();
+//     });
+
+//     archive.finalize();
+//   });
+// }
+
+/**
+ * Uploads the file to Google Drive
+ */
+function uploadToDrive(name, path) {
+  actions.info('Uploading file to Goole Drive...');
+  drive.files.create({
+    requestBody: {
+      name,
+      parents: [folder]
+    },
+    media: {
+      body: fs.createReadStream(path)
+    }
+  })
+  .then(res => {
+    actions.setOutput(fileLink, `https://drive.google.com/file/d/${res.data.id}/view?usp=sharing`)
+    actions.info('File uploaded successfully')
+  })
+  .catch(e => {
+    actions.error('Upload failed~~~~!!!!!');
+    throw e;
+  });
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {OAuth2Client} authClient An authorized OAuth2 client.
+ */
+async function listFiles() {
+  //const drive = google.drive({version: 'v3', auth: authClient});
+  const res = await drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  });
+  const files = res.data.files;
+  if (files.length === 0) {
+    console.log('No files found.');
+    return;
+  }
+
+  console.log('Files:');
+  files.map((file) => {
+    console.log(`${file.name} (${file.id})`);
+  });
+}
+
+/**
+ * Search file in drive location
+ * @return{obj} data file
+ * */
+async function searchFile() {
+  const files = [];
+  try {
+    const res = await drive.files.list({
+      fields: 'nextPageToken, files(id, name)',
+      spaces: 'drive'
+    });
+    Array.prototype.push.apply(files, res.files);
+    res.data.files.forEach(function(file) {
+      console.log('Found file:', file.name, file.id);
+    });
+
+    console.log(res.data.files);
+
+    return res.data.files;
+  } catch (err) {
+    console.log('No files found..');
+    throw err;
+  }
+}
+
+main().catch(e => actions.setFailed(e));
+
+
+/***/ })
+
+/******/ });
